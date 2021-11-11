550b1d9fc89428b57a4b165570898bd40a45e047
K0lb3 <rkolbe96@gmail.com>
2021-11-11 17:28:23 +0100

    StringCipher implemented

--- lib/AESEncryptionService.py
+++ lib/AESEncryptionService.py
@@ -5,8 +5,10 @@ AESKey: bytes = "GameDevelopmentDepartment".encode("utf8")
 AESIV: bytes = "TendouAris".encode("utf8")
 
 def EncryptStringToBytes_Aes(plainText: str, Key: bytes, IV: bytes) -> bytes:
+    # same as StringCipher?
     pass
 
 
 def DecryptStringFromBytes_Aes(cipherText: bytes, Key: bytes, IV: bytes) -> str:
+    # same as StringCipher?
     pass
--- lib/StringCipher.py
+++ lib/StringCipher.py
@@ -1,4 +1,21 @@
 from base64 import b64decode, b64encode
+from Crypto.Protocol.KDF import PBKDF2
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad, unpad
+from Crypto.Random import get_random_bytes
+
+# String *__fastcall TextFileHelper_LoadEncryptedDataFile(String *fileName, String *decryptKey, MethodInfo *method)
+# {
+#   String *AllText; // x0
+
+#   if ( (byte_70F27EB & 1) == 0 )
+#   {
+#     sub_1996CFC(78994LL, (__int64)decryptKey);
+#     byte_70F27EB = 1;
+#   }
+#   AllText = File_ReadAllText(fileName, 0LL);
+#   return StringCipher_Decrypt(AllText, decryptKey, 0LL);
+# }
 
 # // Namespace: MX.Core.Crypto.StringCipher
 BlockSize: int = 128
@@ -7,23 +24,38 @@ DerivationIterations: int = 1000
 
 
 def Encrypt(plainText: str, passPhrase: str) -> str:
-    pass
+    salt = get_random_bytes(0x10) # GenerateRandomEntropy
+    iv = get_random_bytes(0x10) # GenerateRandomEntropy
+    derived = PBKDF2(passPhrase, salt, 16, count=DerivationIterations)
+    cipher = AES.new(key=derived[:16], iv=iv, mode=AES.MODE_CBC)
+    data = pad(cipher.encrypt(plainText.encode("utf8")), BlockSize)
+    return b64encode(salt + iv + data).decode("utf8")
+    
 
 
 def Decrypt(cipherText: str, passPhrase: str) -> str:
     rawCipherText = b64decode(cipherText)
-
-
-def GenerateRandomEntropy() -> bytes:
-    pass
+    salt = rawCipherText[:16]
+    iv = rawCipherText[16:32]
+    rawCipherText = rawCipherText[32:]
+    derived = PBKDF2(passPhrase, salt, 16, count=DerivationIterations)
+    cipher = AES.new(key=derived[:16], iv=iv, mode=AES.MODE_CBC)
+    return unpad(cipher.decrypt(rawCipherText), BlockSize).decode('utf-8')
+    
+# def GenerateRandomEntropy() -> bytes:
+#     pass
 
 
 def EncryptStringToBytes(plainText: str, Key: bytes, IV: bytes) -> bytes:
-    pass
+    # not sure about how the text is encoded, C# directly uses the charcode
+    cipher = AES.new(key=Key, iv=IV, mode=AES.MODE_CBC)
+    return pad(cipher.encrypt(plainText.encode("utf8")), BlockSize)
 
 
 def DecryptStringFromBytes(cipherText: bytes, Key: bytes, IV: bytes) -> str:
-    pass
+    # not sure about how the text is encoded, C# directly uses the charcode
+    cipher = AES.new(key=Key, iv=IV, mode=AES.MODE_CBC)
+    return unpad(cipher.decrypt(cipherText), BlockSize).decode('utf-8')
 
 
 def AESEncrypt256(Input: str, key: str) -> str:
