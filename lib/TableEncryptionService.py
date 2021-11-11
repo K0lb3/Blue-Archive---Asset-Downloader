1a14e04cb669024a9312f882cd4c64941579e695
K0lb3 <rkolbe96@gmail.com>
2021-11-11 23:21:43 +0100

    completition of TableEncryptionService with some help of
    Iza
    アイザヱー#6944

--- lib/TableEncryptionService.py
+++ lib/TableEncryptionService.py
@@ -1,10 +1,15 @@
 from typing import Union
-
+from struct import Struct
 from . import XXHashService
 from .MersenneTwister import MersenneTwister
+from base64 import b64decode, b64encode
 
-uint = ulong = long = int
-double = float
+uint32 = Struct("<I")
+uint64 = Struct("<Q")
+int32 = Struct("<i")
+int64 = Struct("<q")
+float32 = Struct("<f")
+float64 = Struct("<d")
 
 
 def CreateKey(name: str) -> bytes:  # 0x02AAE200-0x02AAE288
@@ -18,31 +23,56 @@ def CreateKey(name: str) -> bytes:  # 0x02AAE200-0x02AAE288
 def XOR(name: str, data: bytes) -> bytes:
     seed = XXHashService.CalculateHash(name)
     twister = MersenneTwister(seed)
-    xor_key = twister.NextBytes(len(data))
+    key = twister.NextBytes(len(data))
 
     if len(data) >= 1:
-        data = bytes(x ^ y for x, y in zip(data, xor_key))
+        data = _XOR(data, key)
     return data
 
 
-def ConvertInt(
-    value: Union[int, long, uint, ulong], key: bytes
-) -> Union[int, long, uint, ulong]:
-    pass
+def _XOR(value: bytes, key: bytes) -> bytes:
+    return bytes(x ^ y for x, y in zip(value, key))
 
 
-def ConvertFloat(value: Union[float, double], key: bytes) -> Union[float, double]:
-    pass
+def _XOR_Struct(value: Union[int, float], key: bytes, struct: str) -> Union[int, float]:
+    return struct.unpack(_XOR(struct.pack(value), key))[0]
 
 
-def EncryptFloat(value: Union[float, double], key: bytes) -> Union[float, double]:
-    pass
+def ConvertInt(value: int, key: bytes) -> int:
+    return _XOR_Struct(value, key, int32) if value else 0
 
 
-def EncryptStr(value: str, key: bytes) -> str:
-    pass
+def ConvertLong(value: int, key: bytes) -> int:
+    return _XOR_Struct(value, key, int64) if value else 0
+
+
+def ConvertUInt(value: int, key: bytes) -> int:
+    return _XOR_Struct(value, key, uint32) if value else 0
+
+
+def ConvertULong(value: int, key: bytes) -> int:
+    return _XOR_Struct(value, key, uint64) if value else 0
+
+
+def ConvertFloat(value: float, key: bytes) -> float:
+    return ConvertInt(int(value), key) * 0.00001 if value else 0.0
+
+
+def ConvertDouble(value: float, key: bytes) -> float:
+    return ConvertLong(int(value), key) * 0.00001 if value else 0.0
+
+
+def EncryptFloat(value: float, key: bytes) -> float:
+    return ConvertInt(int(value * 100000), key) if value else 0.0
+
+
+def EncryptDouble(value: str, key: bytes) -> str:
+    return ConvertLong(int(value * 100000), key) if value else 0.0
 
 
 def ConvertStr(value: str, key: bytes) -> str:
-    pass
+    return _XOR(b64decode(value), key).decode()
 
+
+def EncryptStr(value: str, key: bytes) -> str:
+    return b64encode(_XOR(value.encode(), key))
